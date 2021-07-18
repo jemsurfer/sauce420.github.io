4343de2dd5f6ef92aeffdb1505e3c2d08d0cd8e9
sauce420 <77341102+sauce420@users.noreply.github.com>
2021-07-18 13:26:11 +0100

    Remember theme preference
    store a cookie to remember what theme the user chose and read it whenever the site loads.

---
+++ dark.js
@@ -0,0 +1,53 @@
+light_toggle = false;
+
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = 'expires=' + d.toUTCString();
+  document.cookie = cname + '=' + cvalue + ';' + expires + '; SameSite=Strict';
+}
+
+function getCookie(cname) {
+  let name = cname + '=';
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return '';
+}
+
+check_theme();
+
+function check_theme() {
+  dark_or_not = getCookie('darkmode');
+  if (dark_or_not == "yes") {
+    console.log('darkmode cookie value detected as:', dark_or_not);
+  }
+  if (dark_or_not == "no") {
+    toggle();
+    theme_change();
+    console.log('darkmode cookie value detected as:', dark_or_not);
+  }
+}
+
+function toggle() {
+  light_toggle = !light_toggle;
+}
+
+function theme_change() {
+  if (light_toggle == false) {
+    document.body.classList.toggle('lightmode');
+    setCookie('darkmode', 'yes', 15); //set the cookie and make it expire in 15 days
+  }
+  if (light_toggle == true) {
+    document.body.classList.toggle('lightmode');
+    setCookie('darkmode', 'no', 15); //same as line 29, but with `darkmode = no` because lightmode is toggled
+  }
+}
