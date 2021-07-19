a89b02b773795f006d705654c609fa8b5499d0ec
sauce420 <77341102+sauce420@users.noreply.github.com>
2021-07-19 18:33:09 +0100

    re-arrange javascript
    move functions in javascript to be at the start, it seems more readable.
    remove blank line in css

--- dark.js
+++ dark.js
@@ -1,29 +1,4 @@
-light_toggle = false;
-
-function setCookie(cname, cvalue, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-  let expires = 'expires=' + d.toUTCString();
-  document.cookie = cname + '=' + cvalue + ';' + expires + '; SameSite=Strict';
-}
-
-function getCookie(cname) {
-  let name = cname + '=';
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(';');
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return '';
-}
-
-check_theme();
+light_toggle = false; //darkmode is enabled by default as it should be
 
 function check_theme() {
   dark_or_not = getCookie('darkmode');
@@ -51,3 +26,28 @@ function theme_change() {
     setCookie('darkmode', 'no', 15); //same as line 29, but with `darkmode = no` because lightmode is toggled
   }
 }
+
+function setCookie(cname, cvalue, exdays) { //takes the cookie name, cookie value, and number of days until it should expire as arguments
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = 'expires=' + d.toUTCString();
+  document.cookie = cname + '=' + cvalue + ';' + expires + '; SameSite=Strict';
+}
+
+function getCookie(cname) { //takes the cookie name as an argument
+  let name = cname + '=';
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return '';
+}
+
+check_theme();
